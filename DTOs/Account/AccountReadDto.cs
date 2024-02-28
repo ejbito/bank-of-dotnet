a408323a8dcda7d8ef43cf3b2ef688aa5233ca68
ejbito <ejbito@hotmail.com>
2024-02-28 11:10:32 +1100

    Create DTOs

---
+++ DTOs/Account/AccountReadDto.cs
@@ -0,0 +1,11 @@
+﻿using BankOfDotNet.Enums;
+
+namespace BankofDotNet.DTOs.Account;
+
+public class AccountReadDto
+{
+    public Guid AccountId { get; set; }
+    public Guid UserId { get; set; }
+    public AccountType AccountType { get; set; }
+    public decimal Balance { get; set; }
+}
---
+++ DTOs/User/UserUpdatePasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankofDotNet.DTOs.User;
+
+public class UserUpdatePasswordDto
+{
+    [Required]
+    [DataType(DataType.Password)]
+    public string CurrentPassword { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; }
+}
